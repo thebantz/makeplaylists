8495a9da99d7c7cef2d68d0080eced80791a6445
Nicole <nicoledbanta@gmail.com>
2020-07-05 01:06:17 -0700

    Create an automated Spotify playlist

---
+++ createplaylist.py
@@ -0,0 +1,39 @@
+import json
+import requests
+from secrets.py import spotify_user_id, spotify_token
+
+class CreatePlaylist: 
+  def __init__(self):
+    self.user_id = spotify_user_id
+
+  def getYoutubeClient(self):
+    pass
+
+  def getLikedVideos(self):
+    pass
+
+  def createPlaylist(self):
+    request_body = json.dumps(
+      "name": "Youtube Liked Vids",
+      "description": "All Liked Youtube Videos",
+      "public": True
+    )
+
+    query = "https://api.spotify.com/v1/users/{}/playlists".format(spotify_user_id)
+    response = requests.post(
+      query,
+      data = request_body,
+      headers={
+        "Content-Type":"application/json",
+         "Authorization":"Bearer {}".format(spotify_token)
+      }
+    )
+    response.json = response.json()
+
+    return response_json["id"]
+
+  def getSpotifyUri(self):
+    pass
+
+  def addSongToPlaylist(self):
+    pass
